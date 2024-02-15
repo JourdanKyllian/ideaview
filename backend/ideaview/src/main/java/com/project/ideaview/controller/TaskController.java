9251ee16981af32bcd76a19d84e14d5aabc14b02
KyllianDev <kyllianjourdan@outlook.com>
2024-02-15 09:33:14 +0100

    t

--- backend/ideaview/src/main/java/com/project/ideaview/controller/TaskController.java
+++ backend/ideaview/src/main/java/com/project/ideaview/controller/TaskController.java
@@ -1,7 +1,21 @@
 package com.project.ideaview.controller;
 
+import com.project.ideaview.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/task")
+    public String task(){
+        return "task";
+    }
+
+    @PostMapping("/dashboard/task")
+    public void
 }
