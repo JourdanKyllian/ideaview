fe87a63535bbdff81660f04cbef85e7238b52612
KyllianDev <kyllianjourdan@outlook.com>
2024-04-16 12:29:09 +0200

    start du projet front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/general/not-found/not-found.component';
+import { HomePageComponent } from './pages/general/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomePageComponent
+    },
+    {
+        path: "**",
+        component: NotFoundComponent
+    }
+];
