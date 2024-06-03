a2d90a3fd7982fbf6ada1b7d70da8f0b5acd7000
KyllianDev <kyllianjourdan@outlook.com>
2024-06-03 23:58:29 +0200

    3 juin 2024

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import {NotFoundComponent} from "./pages/general/not-found/not-found.component";
 import {ProfilePageComponent} from "./pages/general/profile-page/profile-page.component";
 import {ProjectPageComponent} from "./pages/general/project-page/project-page.component";
-import {TaskPageComponent} from "./pages/general/task-page/task-page.component";
 import {SuggestionPageComponent} from "./pages/general/suggestion-page/suggestion-page.component";
 import {HomePageComponent} from "./pages/general/home-page/home-page.component";
 import {SignupPageComponent} from "./pages/general/signup-page/signup-page.component";
-import {ChronologyPageComponent} from "./pages/general/chronology-page/chronology-page.component";
 import {NewprojectPageComponent} from "./pages/general/newproject-page/newproject-page.component";
 import {AddpersonPageComponent} from "./pages/general/addperson-page/addperson-page.component";
 import {ProfilesecuritePageComponent} from "./pages/general/profilesecurite-page/profilesecurite-page.component";
@@ -34,10 +32,6 @@ export const routes: Routes = [
     component: SuggestionPageComponent
   },
   {
-    path: "task",
-    component: TaskPageComponent
-  },
-  {
     path: "sign-up",
     component: SignupPageComponent
   },
@@ -48,10 +42,6 @@ export const routes: Routes = [
   {
     path: "new-project",
     component: NewprojectPageComponent
-  },
-  {
-    path: "chronology",
-    component: ChronologyPageComponent
   },
   {
     path: "dashboard",
---
+++ frontend/src/app/models/users.model.ts
@@ -0,0 +1,9 @@
+export class UsersModel {
+  constructor(
+    public id: string,
+    public lastname: string,
+    public firstname: string,
+    public email: string,
+    public password: string
+  ) {}
+}
