a88f28bdb73c754f367ab08518212e1fc320c3e1
KyllianDev <kyllianjourdan@outlook.com>
2024-06-18 21:32:24 +0200

    point fonctionnel

--- backend/ideaview/src/main/java/com/project/ideaview/repository/AdministratorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.ideaview.repository;
-
-import com.project.ideaview.model.Administrator;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdministratorRepository extends JpaRepository<Administrator, Integer> {
-}
